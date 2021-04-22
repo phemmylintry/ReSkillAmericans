a02f51b452508276bb2936082c1ac90baf5b5324
Oluwafemi <phemmylintry@gmail.com>
2021-04-22 01:23:44 +0100

    initial commit

---
+++ classes.py
@@ -0,0 +1,25 @@
+class Budget:
+    
+    #constructor
+    def __init__(self, category, amount): 
+        self.category = category
+        self.amount = amount
+    
+    #methods
+    def deposit(self, amount):
+        pass
+
+
+    def check_balance(self, amount):
+        pass
+
+
+    def withdraw(self, amount):
+        pass
+
+
+    def transfer(self, amount, catergory):
+        pass
+
+
+food_category = Budget("Food", 500)
